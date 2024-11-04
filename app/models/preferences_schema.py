57c2ab9cb3436fd4d6538c81bde37a748c546646
SayeedSanjana <sanjanasayeed68@gmil.com>
2024-11-04 03:23:29 -0500

    visited_places api written

--- app/models/preferences_schema.py
+++ app/models/preferences_schema.py
@@ -1,22 +1,24 @@
-# app/models/preferences_schema.py
-from marshmallow import Schema, fields, ValidationError
+from marshmallow import Schema, fields
 
 class PreferencesSchema(Schema):
-    cuisines = fields.List(fields.String(), required=True, error_messages={"required": "Cuisines are required."})
-    indoor_activities = fields.List(fields.String(), required=True, error_messages={"required": "Indoor activities are required."})
-    outdoor_activities = fields.List(fields.String(), required=True, error_messages={"required": "Outdoor activities are required."})
-    restaurants_visited = fields.List(fields.String(), required=True, error_messages={"required": "Restaurants visited are required."})
-    indoor_places_visited = fields.List(fields.String(), required=True, error_messages={"required": "Indoor places visited are required."})
-    outdoor_places_visited = fields.List(fields.String(), required=True, error_messages={"required": "Outdoor places visited are required."})
-    
-    # preferred_meal_time is a list of dictionaries (each with "meal" and "time")
+    cuisines = fields.List(fields.String(), required=False)
+    indoor_activities = fields.List(fields.String(), required=False)
+    outdoor_activities = fields.List(fields.String(), required=False)
+
+    visited_places = fields.List(
+        fields.Dict(
+            keys=fields.String(validate=lambda x: x in ["unique_id", "name", "address", "category", "rating"]),
+            values=fields.Raw()
+        ),
+        required=False
+    )
+
     preferred_meal_time = fields.List(
         fields.Dict(
-            keys=fields.String(validate=lambda x: x == "meal" or x == "time"),
-            values=fields.String(),
+            keys=fields.String(validate=lambda x: x in ["meal", "time"]),
+            values=fields.String()
         ),
-        required=True,
-        error_messages={"required": "Preferred meal times are required."}
+        required=False
     )
-    
-    other_preferences = fields.List(fields.String(), required=False)  # Optional
+
+    other_preferences = fields.List(fields.String(), required=False)
